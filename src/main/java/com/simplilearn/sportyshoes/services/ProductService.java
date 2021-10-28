63dd0b247c1285514ceddd9c70267adbcc8792a6
Adarsh-Kumar-Mishra <adarshmishra360@gmail.com>
2021-10-28 23:53:17 +0530

    Added database configuration

--- src/main/java/com/simplilearn/sportyshoes/services/ProductService.java
+++ src/main/java/com/simplilearn/sportyshoes/services/ProductService.java
@@ -6,4 +6,8 @@ import com.simplilearn.sportyshoes.entities.Product;
 
 public interface ProductService {
       public List<Product> getProducts();
+      public Product getProduct(long productId);
+      public Product addProduct(Product product );
+      public Product updateProduct(Product product);
+      public void deleteProduct(long productId);
 }
